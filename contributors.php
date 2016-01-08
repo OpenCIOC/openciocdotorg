c4ed6a5f4fc033d2ca289698d718489edef0297b
Katherine Lambacher <kate@kclsoftware.com>
2016-01-08 14:54:33 -0500

    Add contributors page

---
+++ contributors.php
@@ -0,0 +1,57 @@
+<?php
+$strTitle = "Project and Software Contributors";
+$strSection = "Contributors";
+include ("includes/incHeader.php");
+?>
+
+<div class="jumbotron">
+	<div class="container">
+		<h1><?=$strTitle?></h1>
+		<p>
+			We gratefully acknowledge the past and current contributors to CIOC
+			software and the Open CIOC Project. Are you interested in
+			participating in or sponsoring the Open CIOC Project? <a
+				href="http://www.kclsoftware.com/contact-us/">Please contact us!</a>
+		</p>
+	</div>
+</div>
+
+<div class="container">
+	<ul>
+		<li><a href="http://www.211ontario.ca">211 Ontario Services</a>
+		
+		<li>211 Ontario Steering Committee</li>
+		<li>Canadian Taxonomy Project</li>
+		<li>Christopher Lambacher</li>
+		<li><a href="http://www.hamilton.ca">City of Hamilton</a></li>
+		<li><a href="http://www.communityconnection.net">Community Connection
+				- 211 Simcoe County</a></li>
+		<li><a href="http://www.cominfo-ottawa.org">Community Information
+				Centre of Ottawa-Carleton</a></li>
+		<li><a href="http://www.informationhamilton.ca">Community Information
+				Hamilton</a></li>
+		<li><a href="http://www.cioc.ca">Community Information Online
+				Consortium</a></li>
+		<li>Community Information Partners Peel</li>
+		<li><a href="http://www.clbc.ca">Community Living British Columbia</a></li>
+		<li>Connecting Windsor-Essex</li>
+		<li><a href="http://www.hhpl.on.ca">Halton Hills Public Library</a></li>
+		<li><a href="http://www.hipinfo.info">Halton Information Providers</a></li>
+		<li><a href="http://www.findhelp.ca">FindHelp Information Services</a></li>
+		<li><a href="http://www.fourinfo.com">Fourinfo.com</a></li>
+		<li><a href="http://www.informdurham.com">Inform Durham</a> and <a
+			href="http://unitedwaydr.com/">United Way Durham Region</a></li>
+		<li>Katherine Lambacher</li>
+		<li><a href="http://www.kclsoftware.com">KCL Software Solutions Inc.</a></li>
+		<li><a href="http://www.oakville.ca">Town of Oakville</a></li>
+		<li><a href="http://www.trilliumfoundation.org">Ontario Trillium
+				Foundation</a></li>
+		<li>Volunteer @ction Online</li>
+		<li><a href="http://www.volunteerhalton.ca">Volunteer Halton</a></li>
+		<li><a href="http://www.volunteerhamilton.on.ca">Volunteer Hamilton</a></li>
+		<li><a href="http://www.volunteergw.ca">Volunteer Guelph-Wellington</a></li>
+		<li>Walter Lewis</li>
+	</ul>
+</div>
+<?php
+include ("includes/incFooter.php")?>
--- includes/incHeader.php
+++ includes/incHeader.php
@@ -60,6 +60,7 @@
 				</ul>
 			</li>
 			<li<?php if ($strSection=='Resources'): ?> class="active"<?php endif ?>><a href="resources.php">Resources</a></li>
+			<li<?php if ($strSection=='Contributors'): ?> class="active"<?php endif ?>><a href="contributors.php">Contributors</a></li>
             <li><a href="http://www.kclsoftware.com/contact-us/">Contact</a></li>
           </ul>
         </div><!--/.nav-collapse -->
