5dff3a587052683a9537d9bed80590437b1fccb3
Katherine Lambacher <kate@kclsoftware.com>
2016-01-21 23:14:28 -0500

    Update Contributors page

--- contributors.php
+++ contributors.php
@@ -17,41 +17,61 @@ include ("includes/incHeader.php");
 </div>
 
 <div class="container">
-	<ul>
-		<li><a href="http://www.211ontario.ca">211 Ontario Services</a>
-		
-		<li>211 Ontario Steering Committee</li>
-		<li>Canadian Taxonomy Project</li>
-		<li>Christopher Lambacher</li>
-		<li><a href="http://www.hamilton.ca">City of Hamilton</a></li>
-		<li><a href="http://www.communityconnection.net">Community Connection
-				- 211 Simcoe County</a></li>
-		<li><a href="http://www.cominfo-ottawa.org">Community Information
-				Centre of Ottawa-Carleton</a></li>
-		<li><a href="http://www.informationhamilton.ca">Community Information
-				Hamilton</a></li>
-		<li><a href="http://www.cioc.ca">Community Information Online
-				Consortium</a></li>
-		<li>Community Information Partners Peel</li>
-		<li><a href="http://www.clbc.ca">Community Living British Columbia</a></li>
-		<li>Connecting Windsor-Essex</li>
-		<li><a href="http://www.hhpl.on.ca">Halton Hills Public Library</a></li>
-		<li><a href="http://www.hipinfo.info">Halton Information Providers</a></li>
-		<li><a href="http://www.findhelp.ca">FindHelp Information Services</a></li>
-		<li><a href="http://www.fourinfo.com">Fourinfo.com</a></li>
-		<li><a href="http://www.informdurham.com">Inform Durham</a> and <a
-			href="http://unitedwaydr.com/">United Way Durham Region</a></li>
-		<li>Katherine Lambacher</li>
-		<li><a href="http://www.kclsoftware.com">KCL Software Solutions Inc.</a></li>
-		<li><a href="http://www.oakville.ca">Town of Oakville</a></li>
-		<li><a href="http://www.trilliumfoundation.org">Ontario Trillium
-				Foundation</a></li>
-		<li>Volunteer @ction Online</li>
-		<li><a href="http://www.volunteerhalton.ca">Volunteer Halton</a></li>
-		<li><a href="http://www.volunteerhamilton.on.ca">Volunteer Hamilton</a></li>
-		<li><a href="http://www.volunteergw.ca">Volunteer Guelph-Wellington</a></li>
-		<li>Walter Lewis</li>
-	</ul>
+	<div class="row">
+		<div class="col-md-6">
+			<h2>Software Development and Funding</h2>
+			<ul>
+				<li><a href="http://www.211ontario.ca">211 Ontario Services</a>
+				
+				<li>211 Ontario Steering Committee</li>
+				<li>Canadian Taxonomy Project</li>
+				<li>Christopher Lambacher</li>
+				<li><a href="http://www.hamilton.ca">City of Hamilton</a></li>
+				<li><a href="http://www.communityconnection.net">Community
+						Connection - 211 Simcoe County</a></li>
+				<li><a href="http://www.cominfo-ottawa.org">Community Information
+						Centre of Ottawa-Carleton</a></li>
+				<li><a href="http://www.informationhamilton.ca">Community
+						Information Hamilton</a></li>
+				<li><a href="http://www.cioc.ca">Community Information Online
+						Consortium</a></li>
+				<li>Community Information Partners Peel</li>
+				<li><a href="http://www.clbc.ca">Community Living British Columbia</a></li>
+				<li>Connecting Windsor-Essex</li>
+				<li><a href="http://www.hhpl.on.ca">Halton Hills Public Library</a></li>
+				<li><a href="http://www.hipinfo.info">Halton Information Providers</a></li>
+				<li><a href="http://www.findhelp.ca">FindHelp Information Services</a></li>
+				<li><a href="http://www.fourinfo.com">Fourinfo.com</a></li>
+				<li><a href="http://www.informdurham.com">Inform Durham</a> and <a
+					href="http://unitedwaydr.com/">United Way Durham Region</a></li>
+				<li>Katherine Lambacher</li>
+				<li><a href="http://www.kclsoftware.com">KCL Software Solutions Inc.</a></li>
+				<li><a href="http://www.oakville.ca">Town of Oakville</a></li>
+				<li><a href="http://www.trilliumfoundation.org">Ontario Trillium
+						Foundation</a></li>
+				<li>Volunteer @ction Online</li>
+				<li><a href="http://www.volunteerhalton.ca">Volunteer Halton</a></li>
+				<li><a href="http://www.volunteerhamilton.on.ca">Volunteer Hamilton</a></li>
+				<li><a href="http://www.volunteergw.ca">Volunteer Guelph-Wellington</a></li>
+				<li>Walter Lewis</li>
+			</ul>
+		</div>
+		<div class="col-md-6">
+			<h2>Software and Service Providers</h2>
+			<p>Our Open Source project benefits from free and reduced cost
+				software and services from a number of Open-Source friendly vendors,
+				including:</p>
+			<ul>
+				<li><a href="https://github.com/">GitHub</a> Hosted Repository</li>
+				<li><a href="https://bitbucket.org/cioc/ciocweb">BitBucket</a> Hosted Repository</li>
+				<li><a href="https://www.browserstack.com/">BrowserStack</a> Cross-Browser Testing Tools</li>
+				<li><a href="https://www.transifex.com/">Transifex</a> Localization Tools</li>
+			</ul>
+		</div>
+	</div>
+
+
+
 </div>
 <?php
 include ("includes/incFooter.php")?>
